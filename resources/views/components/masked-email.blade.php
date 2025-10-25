67afffd0baae2d331c2b55fb24fea7f8c138aac6
secre159 <axlchanh159@gmail.com>
2025-10-25 19:37:00 +0800

    Users: simplify email masking to 'local@...' per request

--- resources/views/components/masked-email.blade.php
+++ resources/views/components/masked-email.blade.php
@@ -1,16 +1,8 @@
 @php
     $e = $email ?? '';
     if (is_string($e) && strpos($e, '@') !== false) {
-        [$local, $domain] = explode('@', $e, 2);
-        $domainParts = explode('.', $domain);
-        $tld = array_pop($domainParts);
-        $domainCore = implode('.', $domainParts);
-
-        $localVisible = function_exists('mb_substr') ? mb_substr($local, 0, min(3, (function_exists('mb_strlen') ? mb_strlen($local) : strlen($local)))) : substr($local, 0, min(3, strlen($local)));
-        $localMasked = $localVisible . ((function_exists('mb_strlen') ? mb_strlen($local) : strlen($local)) > 3 ? '…' : '');
-
-        $dots = str_repeat('•', max(3, strlen($domainCore)));
-        echo e($localMasked.'@'.$dots.'.'.$tld);
+        [$local] = explode('@', $e, 2);
+        echo e($local . '@...');
     } else {
         echo e($e);
     }
