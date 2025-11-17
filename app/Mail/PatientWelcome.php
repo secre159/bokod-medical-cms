393a623cbedde78e9bef11621fe8c81078fd9671
secre159 <axlchanh159@gmail.com>
2025-11-17 15:16:49 +0800

    Remove ShouldQueue from PatientWelcome to send emails immediately

--- app/Mail/PatientWelcome.php
+++ app/Mail/PatientWelcome.php
@@ -4,13 +4,12 @@ namespace App\Mail;
 
 use App\Models\Patient;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PatientWelcome extends Mailable implements ShouldQueue
+class PatientWelcome extends Mailable
 {
     use Queueable, SerializesModels;
 
